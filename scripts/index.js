d5b7db2293e39d84957517bebeb6181f695d25e7
vmfb-rio <vivienfreitasbrown@gmail.com>
2023-07-06 23:57:48 -0500

    duplicated modal and refactored dupe for cardAdd feat.

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,8 @@ const initialCards = [
   },
 ];
 
+//define variables
+
 const profileEditBtn = document.querySelector("#profileEditBtn");
 const profileEditModal = document.querySelector("#profileEditModal");
 const profileCloseBtn = document.querySelector("#profileCloseBtn");
@@ -35,7 +37,18 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 
+//variables for addcard modal
+
+const cardAddTitle = document.querySelector("#cardAddTitle");
+const cardAddLink = document.querySelector("#cardAddLink");
+const cardAddTitleInput = document.querySelector("#cardAddTitleInput");
+const cardAddLinkInput = document.querySelector("#cardAddLinkInput");
+
+// add new card button
+const addNewCardBtn = document.querySelector(".profile__add-button");
+
 const profileEditForm = document.querySelector(".modal__form");
+const cardAddForm = document.querySelector(".modal__form");
 
 const cardListEl = document.querySelector("#cardsContainer");
 
@@ -43,8 +56,11 @@ const cardTemplate = document
   .querySelector("#card-template")
   .content.querySelector(".card");
 
+//define functions
+
 function closePopup() {
   profileEditModal.classList.remove("modal_opened");
+  cardAddModal.classList.remove("modal_opened");
 }
 
 function handleProfileEditSubmit(e) {
@@ -53,6 +69,15 @@ function handleProfileEditSubmit(e) {
   profileDescription.textContent = profileDescriptionInput.value;
   closePopup();
 }
+
+function handleCardAddSubmit(e) {
+  e.preventDefault();
+  cardAddTitle.textContent = cardAddTitleInput.value;
+  cardAddLink.textContent = cardAddLinkInput.value;
+  closePopup();
+}
+
+// identifies the cards and adds a new card to the end of the list I guess ???
 
 initialCards.forEach((cardData) => {
   const cardElement = cardTemplate.cloneNode(true);
@@ -67,14 +92,21 @@ initialCards.forEach((cardData) => {
   return cardElement;
 });
 
+//event listeners
+
 profileEditBtn.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
-profileCloseBtn.addEventListener("click", closePopup);
-profileEditForm.addEventListener("submit", handleProfileEditSubmit);
-
-initialCards.forEach((cardData) => {
-  cardsWrap.prepend(getCardElement(cardData));
+addNewCardBtn.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  cardAddModal.classList.add("modal_opened");
 });
+
+profileCloseBtn.addEventListener("click", closePopup);
+cardAddCloseBtn.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+cardAddForm.addEventListener("submit", handleCardAddSubmit);
