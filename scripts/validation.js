787f32f9dc1b397154e8811c7902b64ee39ee30f
vmfb-rio <vivienfreitasbrown@gmail.com>
2023-11-19 20:51:32 -0600

    change modal__button_disabled to modal__save_disab

--- scripts/validation.js
+++ scripts/validation.js
@@ -72,7 +72,7 @@ const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__form-input",
   submitButtonSelector: ".modal__save",
-  inactiveButtonClass: "modal__button_disabled",
+  inactiveButtonClass: "modal__save_disabled",
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error"
 };
