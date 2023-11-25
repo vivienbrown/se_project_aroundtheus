6a4778ac6e5c8ec3e803f38d1b55f2032db29d24
vmfb-rio <vivienfreitasbrown@gmail.com>
2023-11-24 19:10:53 -0600

    remove . from last 3 config settings

--- scripts/validation.js
+++ scripts/validation.js
@@ -74,9 +74,9 @@ const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__form-input",
   submitButtonSelector: ".modal__save",
-  inactiveButtonClass: ".modal__save_disabled",
-  inputErrorClass: ".modal__input_type_error",
-  errorClass: ".modal__error"
+  inactiveButtonClass: "modal__save_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error"
 };
 
 enableValidation(config);
