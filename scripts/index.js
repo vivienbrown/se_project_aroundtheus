4ea6c3ccc93c180c0fae74c2f87b913448ad1753
vmfb-rio <vivienfreitasbrown@gmail.com>
2024-02-05 13:51:21 -0600

    handleEscape function

--- scripts/index.js
+++ scripts/index.js
@@ -122,6 +122,20 @@ cardAddCloseBtn.addEventListener("click", () => closePopup(cardAddModal));
 previewCloseBtn.addEventListener("click", () => closePopup(previewImageModal));
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 cardAddForm.addEventListener("submit", handleCardAddSubmit);
+document.addEventListener('keydown', handleEscape);
 
 //index.js set event listeners for click on overlay, keydown esc
-//modal.css check design in figma for accurate color of save button
+
+function handleEscape(evt) {
+  if evt.key = "Escape" {
+    document.querySelector(modal__opened) {
+      closePopup()
+    }
+  };
+}
+
+// when you open a modal, do this
+document.addEventListener('keydown', handleEscape);
+
+// then when you close a modal, remove the keydown listener from the document
+document.removeEventListener...
